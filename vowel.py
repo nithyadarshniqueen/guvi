bd62494abcbb86248f136fb7932c9a5c484528fe
nithyadarshniqueen <42862862+nithyadarshniqueen@users.noreply.github.com>
2019-02-07 14:17:12 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,12 @@
+original =input('Enter a word:')
+word = original.lower()
+first = word[0]
+
+if len(original) > 0 and original.isalpha():
+    if first in 'aeiou':
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print ("invalid")
+    
